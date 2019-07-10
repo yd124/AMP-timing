7e3afa71d178f0abc45cee19509a4cc6d502e020
Fabian <ecojeda@fredhutch.org>
2019-07-10 12:04:08 -0700

    Data for Flor

--- Monolix/R/Data_including_Tcells_Monolix.R
+++ Monolix/R/Data_including_Tcells_Monolix.R
@@ -9,20 +9,19 @@ colnames(datacopy)[7]="y"
 datacopy = cbind(datacopy,rep(1,dim(datacopy)[1]))
 colnames(datacopy)[8]="ytype"
 
+datacopy = cbind(datacopy,rep(0,dim(datacopy)[1]))
+colnames(datacopy)[9]="Cens"
+
+datacopy$VL[which(datacopy$VL<=30)]=30
+datacopy$Cens[which(datacopy$VL<=30)]=1
+
 datanew=c()
 for(ID in IDs){
   
   data_ind = datacopy[which(ID==datacopy$ID),]
-
-  # deleting day 0
-  if (data_ind$VL[which(data_ind$days==0)]>1){
-    data_ind_V = data_ind
-  }
-  else
-  {
-    data_ind_V = data_ind[which(data_ind$days>0),]
-  }
-  data_ind_V$y = log10(data_ind_V$VL)
+  
+  data_ind$days=data_ind$days-min(data_ind$days)
+  data_ind$y = log10(data_ind$VL)
   
   # obtain CD4s and compute log10CD4s
   data_ind_CD4s = data_ind[which(!is.na(data_ind$CD4)),]
@@ -36,7 +35,7 @@ for(ID in IDs){
   
   # add to individual rows
 
-  datanew = rbind(datanew,data_ind_V,data_ind_CD4s,data_ind_CD8s)
+  datanew = rbind(datanew,data_ind,data_ind_CD4s,data_ind_CD8s)
   
 }
 
