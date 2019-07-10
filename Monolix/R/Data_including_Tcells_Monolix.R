047ecd04cce9de271a4d1203817705a0c7a842d9
Fabian <ecojeda@fredhutch.org>
2019-07-10 12:06:34 -0700

    Data for Flor

--- Monolix/R/Data_including_Tcells_Monolix.R
+++ Monolix/R/Data_including_Tcells_Monolix.R
@@ -9,8 +9,11 @@ colnames(datacopy)[7]="y"
 datacopy = cbind(datacopy,rep(1,dim(datacopy)[1]))
 colnames(datacopy)[8]="ytype"
 
+datacopy = cbind(datacopy,rep("VL",dim(datacopy)[1]))
+colnames(datacopy)[9]="ytypeID"
+
 datacopy = cbind(datacopy,rep(0,dim(datacopy)[1]))
-colnames(datacopy)[9]="Cens"
+colnames(datacopy)[10]="Cens"
 
 datacopy$VL[which(datacopy$VL<=30)]=30
 datacopy$Cens[which(datacopy$VL<=30)]=1
@@ -27,12 +30,13 @@ for(ID in IDs){
   data_ind_CD4s = data_ind[which(!is.na(data_ind$CD4)),]
   data_ind_CD4s$y = log10(data_ind_CD4s$CD4)
   data_ind_CD4s$ytype = rep(2, length(data_ind_CD4s$ytype))
+  data_ind_CD4s$ytypeID = rep("CD4s", length(data_ind_CD4s$ytype))
   
   # obtain CD8s and compute log10CD8s
   data_ind_CD8s = data_ind[which(!is.na(data_ind$CD8)),]
   data_ind_CD8s$y = log10(data_ind_CD8s$CD8)
   data_ind_CD8s$ytype = rep(3, length(data_ind_CD8s$ytype))
-  
+  data_ind_CD8s$ytypeID = rep("CD8s", length(data_ind_CD8s$ytype))
   # add to individual rows
 
   datanew = rbind(datanew,data_ind,data_ind_CD4s,data_ind_CD8s)
