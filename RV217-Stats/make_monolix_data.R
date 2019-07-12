9c7f3eff9e4972cb620eeb5f0c51c227c0b8912e
Bryan Mayer <bmayer@fredhutch.org>
2019-07-12 16:18:23 -0700

    updated monolix data format for multiple outcomes

--- RV217-Stats/make_monolix_data.R
+++ RV217-Stats/make_monolix_data.R
@@ -6,11 +6,21 @@ tmp = read_csv("../data/RV217Clean.csv") %>%
   mutate(total = n()) %>%
   subset(total > 2) 
 
-
 tmp %>%
   select(ID, days, log10VL) %>%
   write_csv("../data/RV217Mono.csv")
   
 tmp %>%
   select(ID, days, log10VL, CD4, CD8) %>%
+  gather(ytype_def, y, log10VL, CD4, CD8) %>%
+  mutate(
+    ytype = case_when(
+      ytype_def == "log10VL" ~ 1,
+      ytype_def == "CD4" ~ 2,
+      ytype_def == "CD8" ~ 3
+    )
+  ) %>%
   write_csv("../data/RV217MonoCells.csv")
+
+
+
