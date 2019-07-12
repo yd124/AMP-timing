7b1ffb5954107a12dc04bb7aee201dbde07b5e76
Bryan Mayer <bmayer@fredhutch.org>
2019-07-12 12:43:48 -0700

    added data with cells

--- RV217-Stats/make_monolix_data.R
+++ RV217-Stats/make_monolix_data.R
@@ -1,10 +1,16 @@
 library(tidyverse)
 
-read_csv("../data/RV217Clean.csv") %>%
+tmp = read_csv("../data/RV217Clean.csv") %>%
   subset(!is.na(log10VL) & days >= 0) %>%
   group_by(ID) %>%
   mutate(total = n()) %>%
-  subset(total > 2) %>%
+  subset(total > 2) 
+
+
+tmp %>%
   select(ID, days, log10VL) %>%
   write_csv("../data/RV217Mono.csv")
   
+tmp %>%
+  select(ID, days, log10VL, CD4, CD8) %>%
+  write_csv("../data/RV217MonoCells.csv")
