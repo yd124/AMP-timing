160cabad35e10c89cc3dcd660226e2a850ce2f2e
Bryan Mayer <bmayer@fredhutch.org>
2019-07-12 12:39:45 -0700

    mono data processing and remove single measurement ptid

--- RV217-Stats/make_monolix_data.R
+++ RV217-Stats/make_monolix_data.R
@@ -2,6 +2,9 @@ library(tidyverse)
 
 read_csv("../data/RV217Clean.csv") %>%
   subset(!is.na(log10VL) & days >= 0) %>%
+  group_by(ID) %>%
+  mutate(total = n()) %>%
+  subset(total > 2) %>%
   select(ID, days, log10VL) %>%
   write_csv("../data/RV217Mono.csv")
   
