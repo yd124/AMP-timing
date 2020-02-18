605714c7c4508fdee75bedefa1aaebe867897b56
efabianc <46694146+efabianc@users.noreply.github.com>
2020-02-18 15:16:38 -0800

    Curating the data a little bit more
    Created new R file to built data file to:
    1. Make cens column 1 or 0 instead of TRUE/FALSE.
    2. VLs for censor data equal log10(15)
    3. Deleted data from participant 41146 as it was mostly NAs

---
+++ RV217-Analysis/data-cleaning/make_monolix_data_2.R
@@ -0,0 +1,105 @@
+library(tidyverse)
+
+exclude_ids = c("10502", "40640", "41002")
+
+raw_dat = read_csv("../../data/RV217Clean.csv") 
+
+
+# ----- Censoring ------------
+
+tmp_cens = raw_dat %>%
+  subset(primary_kinetics | days < 0) %>%
+  group_by(ID) %>%
+  mutate(total = n()) %>%
+  subset(total > 2)
+
+tmp_cens %>%
+  mutate(cens = 1*(days < 0) )%>%
+  select(ID, days, log10VL, cens) %>%
+  subset(!ID %in% exclude_ids) %>%
+  write_csv("../../data/RV217MonoCens_2.csv")
+
+
+# ----- No Censoring ------------
+
+tmp = raw_dat %>%
+  subset(!is.na(log10VL) & primary_kinetics) %>%
+  group_by(ID) %>%
+  mutate(total = n()) %>%
+  subset(total > 2)
+
+tmp %>%
+  select(ID, days, log10VL) %>%
+  subset(!ID %in% exclude_ids) %>%
+  write_csv("../../data/RV217Mono_2.csv")
+
+tmp %>%
+  select(ID, days, log10VL, CD4, CD8) %>%
+  subset(!ID %in% exclude_ids) %>%
+  gather(ytype_def, y, log10VL, CD4, CD8) %>%
+  mutate(
+    ytype = case_when(
+      ytype_def == "log10VL" ~ 1,
+      ytype_def == "CD4" ~ 2,
+      ytype_def == "CD8" ~ 3
+    )
+  ) %>%
+  write_csv("../../data/RV217MonoCells_2.csv")
+
+# ----- APTIMA prediction + censoring ------------
+# model comes from the APTIMA-VL analysis
+
+mod_data = raw_dat %>%
+  subset(ID != 40320) %>% # poor data
+  subset(days == 0) %>%
+  select(ID, VL, log10VL, APTIMA_num) %>%
+  na.omit() %>% # complete case subsetting but not actually necessary
+  mutate(
+    ID = factor(ID), 
+    log10_APTIMA = log10(APTIMA_num)
+  )
+
+pred_mod = lm(log10VL ~ APTIMA_num, 
+              data = subset(mod_data, APTIMA_num >= 9 & APTIMA_num <= 34))
+
+raw_dat %>%
+  subset(primary_kinetics | days < 0) %>%
+  group_by(ID) %>%
+  mutate(total = n()) %>%
+  subset(total > 2) %>%
+  mutate(
+    predicted_VL = !is.na(APTIMA_num) & (days < 0 & (APTIMA_num >= 9 & APTIMA_num <= 34)),
+    log10VL_pred = if_else(predicted_VL, 
+                           predict(pred_mod, newdata = data.frame(APTIMA_num = APTIMA_num)),
+                           if_else(is.na(log10VL) & days<0, 
+                                   log10(15),
+                                   log10VL)),
+    log10VL_cens = if_else(is.na(log10VL) & days<0, 
+                           log10(15),
+                           log10VL),
+    cens = 1*(days < 0),
+    cens_pred = 1*(log10VL_pred <= log10(15))
+    ) %>%
+  group_by(ID) %>%
+  mutate(
+    first_pred_day = if(any(predicted_VL)) -(min(-days[days < 0])) else 0,
+    cens_firstpred = days < first_pred_day
+  ) %>%
+  select(ID, days, log10VL, log10VL_cens, cens, APTIMA_num, predicted_VL, log10VL_pred,
+         cens_pred, first_pred_day, cens_firstpred) %>%
+  subset(!ID %in% exclude_ids) %>%
+  write_csv("../../data/RV217MonoVLPred_2.csv")
+
+
+
+
+
+# ----- Compare to Dan's data ------------
+
+dan_data = read_csv("../../Monolix/Data/DBRout_RV217.csv")
+test = tmp %>%
+  subset(days_dx <= 100) %>%
+  select(ID, days_dx, log10VL) %>%
+  left_join(dan_data, by = c("ID", "days_dx" = "days"))
+
+#all(test$log10VL.x == test$log10VL.y, na.rm = T)
